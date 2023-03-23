ba8ed0315d8780fb8c5e77ef6a14af21d91d6c6b
Sathishmahi <sathishmahi433@gmail.com>
2023-03-23 12:27:59 +0530

    flask hello world created

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask
+from flask import Flask,request,jsonify
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def home():
+    return f'<h1>hello world</h1>'
+
+if __name__=='__main__':
+    app.run('0.0.0.0',port=8000,debug=True)
